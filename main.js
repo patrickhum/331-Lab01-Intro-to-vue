31f1a27ec2ee67f08e0230b3f53cb5f035db0652
patrickhum <jirapat_na@cmu.ac.th>
2023-07-29 17:17:07 +0700

    4.9

--- main.js
+++ main.js
@@ -5,10 +5,16 @@ createApp({
         const product = ref('Socks')
         const image = ref('./assets/images/socks_green.jpg')
         const productlink = ref('https://www.camt.cmu.ac.th')
+        const inStock = ref(false)
+        const inventory = ref(5)
+        const onSale = ref(false)
         return {
             product,
             image,
-            productlink
+            productlink,
+            inStock,
+            inventory,
+            onSale
         }
     }
 }) .mount('#app')
