b6db2c393fa5511ae1d1a35cf892e1a651bbba39
patrickhum <jirapat_na@cmu.ac.th>
2023-07-29 19:43:18 +0700

    6.7

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ createApp({
         const image = ref('./assets/images/socks_green.jpg')
         const productlink = ref('https://www.camt.cmu.ac.th')
         const inStock = ref(false)
-        const inventory = ref(5)
+        const inventory = ref(0)
         const onSale = ref(false)
         const details = ref([
             '50% cotton',
@@ -14,14 +14,24 @@ createApp({
             '20% polyester'
         ])
         const variants = ref([
-            { id: 2234, color:'green'},
-            { id: 2235, color:'blue'}
+            { id: 2234, color:'green', image: './assets/images/socks_green.jpg'},
+            { id: 2235, color:'blue', image: './assets/images/socks_blue.jpg'}
         ])
         const sizes = ref([
             'S',
             'M',
             'L'
         ])
+        const cart = ref(0)
+            function addToCart(){
+                cart.value +=1
+            }
+            function updateImage(variantImage){
+                image.value = variantImage
+            }
+            function inStockstatus(){
+                inStock.value =!inStock.value
+            }
         return {
             product,
             image,
@@ -31,7 +41,11 @@ createApp({
             onSale,
             details,
             variants,
-            sizes
+            sizes,
+            cart,
+            addToCart,
+            updateImage,
+            inStockstatus
         }
     }
 }) .mount('#app')
