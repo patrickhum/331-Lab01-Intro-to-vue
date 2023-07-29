bc5fc453531c67244b039fdb30f8a64e4956afc5
patrickhum <jirapat_na@cmu.ac.th>
2023-07-29 17:30:56 +0700

    5.5

--- main.js
+++ main.js
@@ -8,13 +8,30 @@ createApp({
         const inStock = ref(false)
         const inventory = ref(5)
         const onSale = ref(false)
+        const details = ref([
+            '50% cotton',
+            '30% wool',
+            '20% polyester'
+        ])
+        const variants = ref([
+            { id: 2234, color:'green'},
+            { id: 2235, color:'blue'}
+        ])
+        const sizes = ref([
+            'S',
+            'M',
+            'L'
+        ])
         return {
             product,
             image,
             productlink,
             inStock,
             inventory,
-            onSale
+            onSale,
+            details,
+            variants,
+            sizes
         }
     }
 }) .mount('#app')
